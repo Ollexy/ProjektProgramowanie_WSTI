2fc47720606c76019437c0530321e4f06d9c655b
Ollexy <r.oleksiewicz18@gmail.com>
2023-11-11 15:18:14 +0100

    CompanyManagementprogram(program handler) class updated - methods declaration added

--- CompanyManagementProgram.h
+++ CompanyManagementProgram.h
@@ -1,8 +1,6 @@
 #ifndef PROJEKTPROGRAMOWANIE_WSTI_COMPANYMANAGEMENTPROGRAM_H
 #define PROJEKTPROGRAMOWANIE_WSTI_COMPANYMANAGEMENTPROGRAM_H
 
-#include <iostream>
-#include <fstream>
 #include "Person.h"
 #include "Employee.h"
 #include "Client.h"
@@ -11,9 +9,42 @@
 #include "HRDepartment.h"
 #include "MarketingDepartment.h"
 #include "FinanceDepartment.h"
+#include "Bin.h"
+#include "CompanyManagementProgram.h"
+#include "FileManager.h"
+#include <iostream>
+#include <fstream>
 
 class CompanyManagementProgram {
+private:
+    std::vector<Employee> employees;
+    std::vector<Client> clients;
+    Bin bin;
+    FileManager fileManager;
 
+public:
+    CompanyManagementProgram();
+
+    void start();
+    void mainMenu();
+    void secondaryMenu();
+    void displayMenu();
+    void displayLoginScreen();
+
+    void addEmployee();
+    void removeEmployee();
+    void updateEmployeeDetails();
+    void displayEmloyees();
+    void sortEmployees();
+
+    void addClient();
+    void removeClient();
+    void updateClientDetails();
+    void displayClients();
+    void sortClients();
+
+    void saveData();
+    void loadData();
 };
 
 
